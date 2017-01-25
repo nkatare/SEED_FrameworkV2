4c850418ba0ad4a69a36c79ad7972999aed37b7e
Nikhil's Dell <Nikhil's Dell@NikhilXPS13>
2017-01-25 11:39:31 +0000

    Changes on Main input

--- src/javaSeedMain/Main.java
+++ src/javaSeedMain/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InterruptedException {
 
-		String EnvItr = "3";//args[0];
+		String EnvItr = args[0];
 		TriggerJavaSeed.TriggerScenarios(EnvItr);
 		
 	}
