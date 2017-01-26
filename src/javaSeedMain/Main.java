988bd59f231924a7653cf4650ac1b46ca41a079a
Nikhil's Dell <Nikhil's Dell@NikhilXPS13>
2017-01-26 15:22:29 +0000

    Added Main Parameterization code

--- src/javaSeedMain/Main.java
+++ src/javaSeedMain/Main.java
@@ -8,9 +8,22 @@ public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InterruptedException {
 
-		String EnvItr = args[0];
-		TriggerJavaSeed.TriggerScenarios(EnvItr);
+/*		String EnvItr = args[0];
+		TriggerJavaSeed.TriggerScenarios(EnvItr);*/		
+				
+		String EnvParam = args[0].toString();
+		int intEnvItrStart,intEnvItrEnd;		
 		
+		if(EnvParam.contains("-")){
+			String[] arrEnvParam = EnvParam.split("-");
+			intEnvItrStart = Integer.parseInt(arrEnvParam[0]);
+			intEnvItrEnd = Integer.parseInt(arrEnvParam[1]);
+			for(int i=intEnvItrStart;i<=intEnvItrEnd;i++){
+				TriggerJavaSeed.TriggerScenarios(String.valueOf(i));
+			}
+		}else{
+			String EnvItr = EnvParam;
+			TriggerJavaSeed.TriggerScenarios(EnvItr);
+		}
 	}
-
 }
