ff32a50e3e6be29915cf8604ec41216e2cd4051b
Nikhil's Dell <Nikhil's Dell@NikhilXPS13>
2017-01-26 15:57:50 +0000

    Fix: Moved Return Code Logic while reading Failed TC Arraylist after..
    ..each Iterative loop of Environment.

--- src/javaSeedMain/Main.java
+++ src/javaSeedMain/Main.java
@@ -2,12 +2,16 @@ package javaSeedMain;
 
 import java.lang.reflect.InvocationTargetException;
 
+import javaSeed.constants.Const;
 import javaSeed.driverSheet.*;
 
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InterruptedException {
 
+		int ErrorCodeStatus=0;
+		
+		
 /*		String EnvItr = args[0];
 		TriggerJavaSeed.TriggerScenarios(EnvItr);*/		
 				
@@ -20,10 +24,27 @@ public class Main {
 			intEnvItrEnd = Integer.parseInt(arrEnvParam[1]);
 			for(int i=intEnvItrStart;i<=intEnvItrEnd;i++){
 				TriggerJavaSeed.TriggerScenarios(String.valueOf(i));
+				// Piece to read JIRATCKeyFAILLIST Array list for any Failed TC, if any return the Exit Code 1
+				if(Const.JIRATCKeyFAILLIST.size()>=1){
+					ErrorCodeStatus=1;
+				}
+				// End of return the Exit Code 1 piece
 			}
 		}else{
 			String EnvItr = EnvParam;
 			TriggerJavaSeed.TriggerScenarios(EnvItr);
+			// Piece to read JIRATCKeyFAILLIST Array list for any Failed TC, if any return the Exit Code 1
+			if(Const.JIRATCKeyFAILLIST.size()>=1){
+				ErrorCodeStatus=1;
+			}
+			// End of return the Exit Code 1 piece
 		}
+		
+		// Piece to read JIRATCKeyFAILLIST Array list for any Failed TC, if any return the Exit Code 1
+		if(Const.JIRATCKeyFAILLIST.size()>=1){
+			ErrorCodeStatus=1;
+		}
+		
+		System.exit(ErrorCodeStatus);
 	}
 }
